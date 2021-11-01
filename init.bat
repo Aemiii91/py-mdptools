baa40a23e95c2439c5e81b41f4ab3a7e84ea5c00
mholdg16 <mholdg16@student.aau.dk>
2021-11-01 12:44:27 +0100

    Edit

--- init.bat
+++ init.bat
@@ -1,5 +1,5 @@
-py -m venv ./env
-.\env\Scripts\activate
-py -m pip install --upgrade pip
-py -m pip install -r ./requirements.txt
+py -m venv env && ^
+.\env\Scripts\activate & ^
+py -m pip install --upgrade pip & ^
+py -m pip install -r requirements.txt & ^
 py -m pip install -e . --no-deps
