42e7177af907e8906caf7edc5c6f2ec2fedd5c1e
mholdg16 <mholdg16@student.aau.dk>
2021-11-01 09:22:53 +0100

    Linting

--- vscode.py
+++ vscode.py
@@ -6,9 +6,13 @@ at_root = lambda filename: path.join(path.dirname(__file__), filename)
 with open(at_root("README.md"), encoding="utf-8") as f:
     readme = f.read()
 
-vscode_files_re = r"`\.vscode\/(.*?)\.json`\n+```(?:json|jsonc)\n((?:.|\n)*?)\n```"
+vscode_files_re = (
+    r"`\.vscode\/(.*?)\.json`\n+```(?:json|jsonc)\n((?:.|\n)*?)\n```"
+)
 vscode_files = re.findall(vscode_files_re, readme)
 
 for filename, content in vscode_files:
-    with open(at_root(f".vscode/{filename}.json"), "w+", encoding="utf-8") as f:
+    with open(
+        at_root(f".vscode/{filename}.json"), "w+", encoding="utf-8"
+    ) as f:
         f.write(content)
